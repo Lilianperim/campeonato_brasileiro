8f4f265db5411ef3ef330930b43e5d4f08524b26
Lilian Cristina <liliancrisperim@gmail.com>
2021-03-10 17:03:27 -0300

    versao inicial

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+  <AppTopBar></AppTopBar>
+  
+    <v-main>
+    <Inicio></Inicio>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Inicio from './components/Inicio'
+import AppTopBar from './components/shared/AppTopBar'
+
+export default {
+  name: 'App',
+  components:{
+      Inicio,
+      AppTopBar
+  },
+ 
+};
+</script>
